79ed3bce2aa532b63b7e9208ded85a7467f5b423
Devarsh-Anilkumar <devarshanilkumar0@gmail.com>
2022-07-07 21:58:47 +0530

    Aaaaaaaaaahhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ Useless_Height_Calculator.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+print('--------Useless_Height_Calculator--------')
+print(" ")
+height = input("Input your height: ")
+unit = input("Unit? ")
+print("Your height is " + height + unit)
