79288e43d4e9b0258b310a47573c0e376f699abd
Devarsh-Anilkumar <devarshanilkumar0@gmail.com>
2022-08-06 12:09:03 +0530

    Removed _s

--- Useless_Height_Calculator.py
+++ Useless_Height_Calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-print('\n--------Useless_Height_Calculator--------')
+print('\n--------Useless Height Calculator--------')
 height = input("\nInput your height: ")
 unit = input("Unit? ")
 print("Your height is " + height + unit)
